fff0d2e1c5b6bd3c573572f6bd4099fb0aadb102
julianty <alexanderjulianty@gmail.com>
2022-09-20 10:19:49 -0700

    Implements a method to create draggable elements

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
 import Header from './Components/Header';
 import Sidebar from './Components/Sidebar';
+import dragElement from './Components/dragElement';
+import { useEffect } from 'react';
+
 
 function App() {
+  useEffect(() => {
+    dragElement(document.getElementById('draggable'));
+    console.log(document.getElementById('draggable'));
+  }, []);
   const style = {
     display: 'grid',
     gridTemplateRows: '5% auto',
@@ -14,7 +21,7 @@ function App() {
     <div style={style}>
       <Header />
       <Sidebar />
-
+      <div id='draggable'>Hello</div>
     </div>
   );
 }
