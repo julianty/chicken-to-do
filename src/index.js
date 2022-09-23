992470ba2b22ddb818bc00d3f0631609e9f34e4b
julianty <alexanderjulianty@gmail.com>
2022-09-22 22:27:12 -0700

    Initializes firebase

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB5WY6q3351Eby88T9Lhlo4wMaZEnKfPng",
+  authDomain: "chicken-tasks.firebaseapp.com",
+  projectId: "chicken-tasks",
+  storageBucket: "chicken-tasks.appspot.com",
+  messagingSenderId: "273950036329",
+  appId: "1:273950036329:web:02ff88e208dbf3df3bedf8"
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -11,6 +26,8 @@ root.render(
   </React.StrictMode>
 );
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
