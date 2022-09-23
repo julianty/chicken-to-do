ad279d7160ec47951ea3bb330f9133465ea10e78
julianty <alexanderjulianty@gmail.com>
2022-09-22 21:56:57 -0700

    Adds default dummy data to Card

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,6 +4,10 @@ import { Component } from "react"
 class Card extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      uid: 'admin',
+      created: '00:00:00'
+    }
   }
   render = () => {
     return (
